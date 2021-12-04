0f71ea89f5d3b6314d886981650fb077ab7a07bc
Josh Carp <jm.carp@gmail.com>
2021-12-04 12:08:27 -0500

    Pin requirements and handle breaking change.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import calendar
 import datetime
 import logging
 import re
@@ -18,8 +19,7 @@ IMG_PATTERN = re.compile(r"images/([b\d])[a-z]\.gif", re.IGNORECASE)
 
 
 def get_spaces(lot: str) -> int:
-    url = f"{BASE_URL}?lotname={lot}"
-    resp = requests.get(url)
+    resp = requests.get(BASE_URL, params={"lotname": lot})
     resp.raise_for_status()
     doc = lxml.html.fromstring(resp.content)
     digits = [
@@ -39,11 +39,11 @@ def image_to_digit(url: str) -> str:
 
 
 def record_metrics(client, project_id: str, rows: List[Dict], timestamp: float):
-    project_path = client.project_path(project_id)
+    project_path = client.common_project_path(project_id)
     series = [
         record_metric_series(row["lot"], row["spaces"], timestamp) for row in rows
     ]
-    client.create_time_series(project_path, series)
+    client.create_time_series(request={"name": project_path, "time_series": series})
 
 
 def record_metric_series(
@@ -53,12 +53,15 @@ def record_metric_series(
     series.metric.type = "custom.googleapis.com/spaces"
     series.resource.type = "global"
     series.metric.labels["lot"] = lot
-    point = series.points.add()
-    point.value.int64_value = spaces
-    point.interval.end_time.seconds = int(timestamp)
-    point.interval.end_time.nanos = int(
-        (timestamp - point.interval.end_time.seconds) * 10 ** 9
+    seconds = int(timestamp)
+    nanos = int((timestamp - seconds) * 10 ** 9)
+    interval = monitoring_v3.TimeInterval(
+        {"end_time": {"seconds": seconds, "nanos": nanos}}
     )
+    point = monitoring_v3.Point(
+        {"interval": interval, "value": {"int64_value": spaces}}
+    )
+    series.points = [point]
     return series
 
 
@@ -80,7 +83,9 @@ def update_spaces(event, context) -> None:
     table = bigquery_client.get_table(f"{project_id}.{dataset_id}.{table_id}")
     errors = bigquery_client.insert_rows(table, rows)
 
-    record_metrics(metrics_client, project_id, rows, timestamp.timestamp())
+    record_metrics(
+        metrics_client, project_id, rows, calendar.timegm(timestamp.timetuple())
+    )
 
     if len(errors) > 0:
         raise RuntimeError(errors)
